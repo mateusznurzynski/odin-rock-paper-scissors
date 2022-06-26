ed9ae7df3a290968dd052436f7c95ee8e72de0fc
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-06-26 22:37:18 +0200

    Add a function that starts a round

--- main.js
+++ main.js
@@ -1,16 +1,53 @@
+const ROCK = {
+  beats: 'scissors',
+  losesTo: 'paper',
+};
+const PAPER = {
+  beats: 'rock',
+  losesTo: 'scissors',
+};
+const SCISSORS = {
+  beats: 'paper',
+  losesTo: 'rock',
+};
+
 function getComputerChoice() {
   let computerChoice;
   let choiceNum = Math.floor(Math.random() * 3) + 1;
   switch (choiceNum) {
     case 1:
-      computerChoice = 'rock';
+      computerChoice = ROCK;
       break;
     case 2:
-      computerChoice = 'paper';
+      computerChoice = PAPER;
       break;
     case 3:
-      computerChoice = 'scissors';
+      computerChoice = SCISSORS;
   }
   return computerChoice;
 }
-console.log(getComputerChoice());
+
+function playRound(playerChoice, computerChoice) {
+  playerChoice = playerChoice.toLowerCase();
+  let verdict;
+  if (playerChoice === computerChoice) {
+    verdict = `Tie! You both chose ${playerChoice}`;
+  } else {
+  }
+}
+
+function play(choice) {
+  switch (choice) {
+    case 'rock':
+      playRound(ROCK, getComputerChoice());
+      break;
+    case 'paper':
+      playRound(PAPER, getComputerChoice());
+      break;
+    case 'scissors':
+      playRound(SCISSORS, getComputerChoice());
+      break;
+    default:
+      console.log('Please use rock, paper or scissors');
+  }
+}
