d83bbf3922c7d030cf9c7580577a2d3537abfabd
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-06-26 22:52:31 +0200

    Make the game fully work in console
    To launch the game, use play('choice'), where choice is rock,
    paper or scissors.
    
    If you type something else as a choice, you will be asked to use
    one of the correct options.

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const ROCK = {
   beats: 'scissors',
   losesTo: 'paper',
+  name: 'Rock',
 };
 const PAPER = {
   beats: 'rock',
   losesTo: 'scissors',
+  name: 'Paper',
 };
 const SCISSORS = {
   beats: 'paper',
   losesTo: 'rock',
+  name: 'Scissors',
 };
 
 function getComputerChoice() {
@@ -27,27 +30,35 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function playRound(playerChoice, computerChoice) {
-  playerChoice = playerChoice.toLowerCase();
+function getVerdict(playerChoice, computerChoice) {
   let verdict;
   if (playerChoice === computerChoice) {
-    verdict = `Tie! You both chose ${playerChoice}`;
+    verdict = `Tie! You both chose ${playerChoice.name}`;
   } else {
+    if (playerChoice.beats === computerChoice.losesTo) {
+      verdict = `You lost! ${playerChoice.name} loses to ${computerChoice.name}`;
+    } else {
+      verdict = `You won! ${playerChoice.name} beats ${computerChoice.name}`;
+    }
   }
+  return verdict;
 }
 
 function play(choice) {
+  let verdict;
+  choice = choice.toLowerCase();
   switch (choice) {
     case 'rock':
-      playRound(ROCK, getComputerChoice());
+      verdict = getVerdict(ROCK, getComputerChoice());
       break;
     case 'paper':
-      playRound(PAPER, getComputerChoice());
+      verdict = getVerdict(PAPER, getComputerChoice());
       break;
     case 'scissors':
-      playRound(SCISSORS, getComputerChoice());
+      verdict = getVerdict(SCISSORS, getComputerChoice());
       break;
     default:
       console.log('Please use rock, paper or scissors');
   }
+  return verdict;
 }
